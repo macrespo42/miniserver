f2818e6002beb91d599fcdab2d2d3978709bc1de
macrespo42 <macrespo@student.42.fr>
2024-10-21 19:17:33 +0200

    feat: handling serving image

--- main.go
+++ main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
+
+func handlerImage(w http.ResponseWriter, r *http.Request) {
+	buf, err := os.ReadFile("./assets/logo.png")
+	if err != nil {
+		log.Fatal("can't read file")
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buf)))
+	w.Write(buf)
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("GET /assets/logo.png", handlerImage)
 
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("can't start miniserver: ", err.Error())
-	}
+	log.Fatal(server.ListenAndServe())
 }
