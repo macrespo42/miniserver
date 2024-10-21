04a50d430f0f0a10ebf24dcdc2c65a33f7031683
macrespo42 <macrespo@student.42.fr>
2024-10-21 16:34:48 +0200

    feat: start listening to :8080

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	router := http.NewServeMux()
+	server := http.Server{
+		Handler: router,
+		Addr:    ":8080",
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("can't start miniserver: ", err.Error())
+	}
+
+	fmt.Printf("listening on http://localhost%s\n", server.Addr)
 }
