654ac04150a8984710f8005c9a41a21b63314310
macrespo42 <macrespo@student.42.fr>
2024-10-22 11:02:46 +0200

    feat: healtz checker route

--- main.go
+++ main.go
@@ -18,10 +18,18 @@ func handlerImage(w http.ResponseWriter, r *http.Request) {
 	w.Write(buf)
 }
 
+func handlerHealth(w http.ResponseWriter, _ *http.Request) {
+	body := []byte("OK")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write(body)
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.HandleFunc("GET /assets/logo.png", handlerImage)
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("GET /app/assets/logo.png", handlerImage)
+	mux.HandleFunc("GET /healthz", handlerHealth)
 
 	server := http.Server{
 		Handler: mux,
