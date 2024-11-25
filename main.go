b28c4f38f265f62fdc624ff5c115af535109a25c
macrespo42 <macrespo@student.42.fr>
2024-11-25 17:57:39 +0100

    feat: restrict http verbs for current routes

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleServerHits(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileServerHits.Load()
+	body := []byte(fmt.Sprintf("Hits: %d", hits))
+	w.WriteHeader(200)
+	w.Write(body)
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+	hits := cfg.fileServerHits.Load()
+	body := []byte(fmt.Sprintf("Hit: %d", hits))
+	w.WriteHeader(200)
+	w.Write(body)
+}
 
 func handlerImage(w http.ResponseWriter, r *http.Request) {
 	buf, err := os.ReadFile("./assets/logo.png")
@@ -26,10 +54,14 @@ func handlerHealth(w http.ResponseWriter, _ *http.Request) {
 }
 
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	rootHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(rootHandler))
 	mux.HandleFunc("GET /app/assets/logo.png", handlerImage)
 	mux.HandleFunc("GET /healthz", handlerHealth)
+	mux.HandleFunc("GET /metrics", apiCfg.handleServerHits)
+	mux.HandleFunc("POST /reset", apiCfg.handleReset)
 
 	server := http.Server{
 		Handler: mux,
