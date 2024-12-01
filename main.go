0f00d29bd15c62b8597b81002633b167c4502074
macrespo42 <macrespo@student.42.fr>
2024-12-01 01:15:56 +0100

    feat: setup sqlc with go

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/macrespo42/miniserver/internal/database"
 )
 
 func main() {
+	godotenv.Load()
+	dbUrl := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Error when connection to database")
+	}
+	dbQueries := database.New(db)
+	fmt.Println(dbQueries)
+
 	apiCfg := ApiConfig{}
 	mux := http.NewServeMux()
 	rootHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
